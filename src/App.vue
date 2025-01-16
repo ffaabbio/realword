87606dc9f2434e55917c6b0c96c35915bfe4f2c5
Suussonic <150890672+Suussonic@users.noreply.github.com>
2025-01-16 15:32:16 +0100

    css

--- src/App.vue
+++ src/App.vue
@@ -15,3 +15,5 @@ export default {
   },
 };
 </script>
+
+<style src="./css/App.css"></style>
--- src/views/UserSignup.vue
+++ src/views/UserSignup.vue
@@ -34,3 +34,5 @@ export default defineComponent({
   },
 });
 </script>
+
+<style src="../css/UserSignup.css"></style>
